f16d92375e2deb8006c1ae2375704d0af290d4b8
Mohak <mohakchamp@gmail.com>
2023-05-12 20:48:17 +0530

    Created backend projects

---
+++ SocialSphere.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace SocialSphere.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Method intentionally left empty, will be covered later
+        }
+    }
+}
