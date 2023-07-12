e7b87c167af290f7150d7199ba3389ac53e2f6cd
Mohak <mohakchamp@gmail.com>
2023-07-12 23:53:13 +0530

    Added github actions checks

--- SocialSphere.UI/src/app/app.component.spec.ts
+++ SocialSphere.UI/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,11 +12,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
@@ -31,4 +34,11 @@ describe('AppComponent', () => {
     const h1Element = fixture.nativeElement.querySelector('h1') as HTMLElement;
     expect(h1Element.textContent).toContain('Social-Sphere UI app is working!!!');
   });
+
+  it('should contain the app-chat component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('app-chat')).toBeTruthy();
+  });
+
 });
--- SocialSphere.UI/src/app/core/components/chat/chat.component.spec.ts
+++ SocialSphere.UI/src/app/core/components/chat/chat.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { ChatComponent } from './chat.component';
 
 describe('ChatComponent', () => {
@@ -8,6 +8,9 @@ describe('ChatComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [ ChatComponent ]
     })
     .compileComponents();
