a0111aa82c8c46ffb3be496ba28ac8276d473880
Mohak <mohakchamp@gmail.com>
2023-07-23 18:51:59 +0530

    Test

--- SocialSphere.UI/src/app/core/services/signalR.service.ts
+++ SocialSphere.UI/src/app/core/services/signalR.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import * as signalR from '@microsoft/signalr';
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { Workbook } from 'exceljs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class SignalRService {
   constructor() {
     this.hubConnection = new HubConnectionBuilder()
       .withUrl('https://localhost:5000/chathub')
-      .configureLogging(signalR.LogLevel.Debug)
+      .configureLogging(LogLevel.Debug)
       .build();
   }
 
@@ -29,5 +29,33 @@ export class SignalRService {
 
   public sendMessage(user: string, message: string): Promise<void> {
     return this.hubConnection.invoke('SendMessage', user, message);
+  }
+
+
+  exportToExcel() {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Sheet 1');
+
+    // Add some data to the worksheet
+    worksheet.addRow(['Name', 'Age', 'Country']);
+    worksheet.addRow(['John', 30, 'USA']);
+    worksheet.addRow(['Alice', 25, 'Canada']);
+    worksheet.addRow(['Bob', 35, 'UK']);
+
+    // Set column headers bold
+    worksheet.getRow(1).font = { bold: true };
+
+    // Generate the Excel file and download it
+    workbook.xlsx.writeBuffer().then((data: ArrayBuffer) => {
+      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'export.xlsx';
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    });
   }
 }
