0b135d8ea8918232f20c9e4ae3e69ce80704dd0e
Mohak Gogia <mohakchamp@gmail.com>
2023-08-05 12:51:30 +0530

    Modified github actions workflows (#15)
    * backend UT workflow
    
    * breaking changes test
    
    * Fixed all 3
    
    * Testing other 2 workflows
    
    * changed
    
    * try
    
    * minor change
    
    * test
    
    * test
    
    * Last try
    
    * Worked

--- SocialSphere.Tests/UnitTest1.cs
+++ SocialSphere.Tests/UnitTest1.cs
@@ -1,12 +1,21 @@
-namespace SocialSphere.Tests
+namespace SocialSphere.Tests;
+
+[TestClass]
+public class UnitTest1
 {
-    [TestClass]
-    public class UnitTest1
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            // Method intentionally left empty, will be covered later
-        }
+        // Method intentionally left empty, will be covered later
+        var val = 1;
+        Assert.AreEqual(1, val);
+    }
+
+    [TestMethod]
+    public void TestMethod2()
+    {
+        // Method intentionally left empty, will be covered later
+        var val = 2;
+        Assert.AreEqual(2, val);
     }
 }
