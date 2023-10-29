ec545aa8508b98cdb0ee7b944d2a7a30f5aaca8e
Mohak Gogia <MohakG@emids.com>
2023-10-29 19:54:18 +0530

    created seperate envs for UI project

---
+++ SocialSphere.UI/src/app/core/services/configuration/configuration.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+export interface IServerConfiguration {
+  ApiAddress: string;
+  IdentityServerAddress: string;
+  AppAddress: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigurationService {
+
+  private configuration!: IServerConfiguration;
+
+  loadConfig() {
+    this.configuration = {
+      ApiAddress: environment.apiAddress,
+      IdentityServerAddress: environment.identityServerAddress,
+      AppAddress: environment.appAddress,
+    };
+  }
+
+  get apiAddress() {
+    return this.configuration.ApiAddress;
+  }
+
+  get identityServerAddress() {
+    return this.configuration.IdentityServerAddress;
+  }
+
+  get appAddress() {
+    return this.configuration.AppAddress;
+  }
+}
+
